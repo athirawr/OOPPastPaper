b8d46c28ff0060e06e1cf8ec7fcd1d9d0628f345
athirawr <126443165+athirawr@users.noreply.github.com>
2023-04-09 15:05:07 +0800

    Create Main.java

---
+++ Question 3/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Desk desk = new Desk(10,20);
+
+        desk.setWidth(40);
+        desk.setHeight(50);
+        desk.setPrice(200);
+
+        desk.printInfo();
+
+    }
+}
