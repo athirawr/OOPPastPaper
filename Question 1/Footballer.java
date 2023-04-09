e713ea7081fe7be328d051013519def82db3afde
athirawr <126443165+athirawr@users.noreply.github.com>
2023-04-09 14:46:45 +0800

    Add files via upload

---
+++ Question 1/Footballer.java
@@ -0,0 +1,30 @@
+public class Footballer {
+
+    private String name;
+    private int numGoals;
+
+    public Footballer(String name){
+        this.name = name;
+    }
+
+    public void addGoal(int num){
+        numGoals = num;
+        numGoals++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumGoals() {
+        return numGoals;
+    }
+
+    public void setNumGoals(int numGoals) {
+        this.numGoals = numGoals;
+    }
+}
